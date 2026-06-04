ab546d8976f641a0d07b32e45f40d980230561e9
cg2025 <93668509+cg2025@users.noreply.github.com>
2026-06-03 20:43:43 -0400

    Delete experiments directory

--- experiments/scaling_analysis.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    for size in 1..=10 {
-        println!("Board size: {}", size);
-
-        // placeholder for state count
-        let states = size * size * size;
-        println!("Estimated states: {}", states);
-    }
-}
